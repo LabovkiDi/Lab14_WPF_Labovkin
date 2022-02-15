2b6f8181ab0f948ff9ab465a8a4a430a339aec80
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-02-16 00:14:25 +0300

    Добавьте файлы проекта.

---
+++ Lab14_WPF_Labovkin/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14_WPF_Labovkin
+{
+   
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Апельсины",
+                ProductPrice = 120,
+                ProductImage = "Data/Апельсины.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Томаты",
+                ProductPrice = 90,
+                ProductImage = "Data/Томаты.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Яблоки",
+                ProductPrice = 70,
+                ProductImage = "Data/Яблоки.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "СВЧ печь",
+                ProductPrice = 6000,
+                ProductImage = "Data/СВЧ_печь.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Холодильник",
+                ProductPrice = 30000,
+                ProductImage = "Data/Холодильник.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Пылесос",
+                ProductPrice = 10000,
+                ProductImage = "Data/Пылесос.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Lab14_WPF_Labovkin/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14_WPF_Labovkin
+{
+
+    class Product
+    {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public ProductCategorys ProductCategory { get; set; }
+    }
+    public enum ProductCategorys
+    {
+        Food,
+        Appliances
+    }
+}
